0a5e084f20244d2af2876f71d675e4b2fda172bf
Ramon <Rahmon@users.noreply.github.com>
2017-07-25 13:35:26 -0300

    Add post navigation

--- single.php
+++ single.php
@@ -20,7 +20,19 @@ get_header(); ?>
 					 * (where ___ is the post format) and that will be used instead.
 					 */
 					get_template_part( 'content', get_post_format() );
-
+					?>
+					<nav class="navigation post-navigation" role="navigation">
+						<h2 class="screen-reader-text"><?php esc_html_e( 'Post navigation', 'avalon-b' ); ?></h2>
+						<div class="nav-links">
+							<div class="nav-previous">
+								<?php previous_post_link( '<span class="meta-nav dashicons dashicons-arrow-left-alt"></span> %link' ); ?>
+							</div>
+							<div class="nav-next">
+								<?php next_post_link( '<span class="meta-nav dashicons dashicons-arrow-right-alt"></span> %link' ); ?>
+							</div>
+						</div>
+					</nav>
+					<?php
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) :
 						comments_template();
