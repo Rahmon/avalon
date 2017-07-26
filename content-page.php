a2f086d3ecd2ff78ff466da7fd0186358c851a24
Ramon <Rahmon@users.noreply.github.com>
2017-07-26 18:52:06 -0300

    Add edit link

--- content-page.php
+++ content-page.php
@@ -21,4 +21,19 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
+	<?php if ( get_edit_post_link() ) : ?>
+		<footer class="entry-footer">
+			<?php
+				edit_post_link(
+					sprintf(
+						/* translators: %s: Name of current post */
+						esc_html__( 'Edit %s', 'avalon-b' ),
+						the_title( '<span class="screen-reader-text">"', '"</span>', false )
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+			?>
+		</footer><!-- .entry-footer -->
+	<?php endif; ?>
 </article><!-- #post-## -->
