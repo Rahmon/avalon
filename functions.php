65c76687899457721aacf1bc3fde5da20e90b303
Ramon <Rahmon@users.noreply.github.com>
2017-09-19 10:39:24 -0300

    Fix name of function used

--- functions.php
+++ functions.php
@@ -228,7 +228,7 @@ require_once get_template_directory() . '/inc/template-tags.php';
 /**
  * WooCommerce compatibility files.
  */
-if ( is_woocommerce_activated() ) {
+if ( avalon_b_is_woocommerce_activated() ) {
 	add_theme_support( 'woocommerce' );
 	require get_template_directory() . '/inc/woocommerce/hooks.php';
 	require get_template_directory() . '/inc/woocommerce/functions.php';
